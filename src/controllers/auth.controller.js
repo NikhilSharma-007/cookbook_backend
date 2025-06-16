5bc2d14d2a71b53d3b24cb7e0df0fdaf15b8ab6c
NikhilSharma-007 <niks21902@gmail.com>
2025-06-17 00:54:41 +0530

    different origin issue fixed

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -91,7 +91,7 @@ const loginUser = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: true,
-    sameSite: "strict",
+    sameSite: "None",
   };
 
   return res
